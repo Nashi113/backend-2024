3faba3372e766841e24bfb9bfd8c71ab7f920144
Nashi113 <ikuameru12@gmail.com>
2024-10-17 15:56:07 +0700

    Praktikum week3

---
+++ percabangan.php
@@ -0,0 +1,5 @@
+<?php
+$nilai = 91;
+if ($nilai > 90){
+    echo 'peringkat anda A';
+}
---
+++ tipedata.php
@@ -0,0 +1,13 @@
+<?php
+$nama = 'Muhammad Ihsan';   //String
+$umur = 27;                 //integer
+$ipk = 3.80;                //float
+$isMarried = false;         //boolean
+
+echo $nama;
+echo '<br>';
+echo $umur;
+echo '<br>';
+echo $ipk;
+echo '<br>';
+echo $isMarried;
