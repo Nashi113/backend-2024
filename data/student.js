75fbc140ab3db146c674f151129f9edf107b3c61
Nashi113 <ikuameru12@gmail.com>
2025-01-03 13:43:15 +0700

    Praktikum Week11

---
+++ data/student.js
@@ -0,0 +1,21 @@
+// TODO 1: Buat data students
+const students = [
+    {
+      id: 1,
+      name: "John Doe",
+      age: 20,
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      age: 21,
+    },
+    {
+      id: 3,
+      name: "Smith Doe",
+      age: 22,
+    },
+  ];
+  
+  // TODO 2: export data students
+  export default students;
